c01c07bb037899387c6423151b81e8f5e7e79fae
Alan Joseph <alan.joseph@myntra.com>
2015-04-25 16:18:42 +0530

    App structure

---
+++ server.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var request = require('request');
+var express = require('express');
+var app = express();
+var _ = require('underscore');
+var path = require('path');
+var bodyParser = require('body-parser');
+var favicon = require('static-favicon');
+
+app.use(favicon());
+
+//var routes = require('./routes/index');
+//routes(app); // attach all routes
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var middleware = [express.urlencoded(), express.json()];
+
+process.argv.forEach(function(val, index, array) {
+    if (val.indexOf('=') !== -1) {
+        var arg = val.split("=");
+        process.env[arg[0]] = arg[1];
+    }
+});
+
+var proxyServer = +process.env['-from'] || +process.env['NODE_PORT'] || 8888;
+
+app.get('/getsecret', [function (req, res) {
+    res.send('secretdata');
+}]);
+
+
+http.createServer(app).listen(proxyServer, function(){
+	console.log('Proxyserver started at ' + proxyServer);
+});
