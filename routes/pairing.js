582062224d12d40217310298d0ac4364e605de31
Jeevan Kleenankandy <jeevan.kk@myntra.com>
2015-04-25 17:22:53 +0530

    added pair

--- routes/pairing.js
+++ routes/pairing.js
@@ -2,9 +2,19 @@
 
 var express = require('express');
 var router = express.Router();
+var Pair = require("../pair");
 
-router.get('/clustering', function(req, res) {
-	
+router.get('/key', function(req, res) {
+	res.json({key: Pair.getKey()});
 });
+
+router.post('/connect', function(req, res) {
+	console.log(req.body);
+	var status = Pair.connect(req.body);
+	res.json({
+		status: status
+	});
+});
+
 
 module.exports = router;
